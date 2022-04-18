701c0ed311784a16ef84e87004c451ad99080d5e
TaygaHoshi <85090948+TaygaHoshi@users.noreply.github.com>
2022-04-18 18:05:22 +0300

    Added button and new activity

---
+++ app/src/main/java/com/example/givasong/AddForm.kt
@@ -0,0 +1,18 @@
+package com.example.givasong
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class AddForm  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_music_layout)
+    }
+
+}
