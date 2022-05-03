a39edf54c64448d502de7e97f4832b865b8b9d5d
TaygaHoshi <85090948+TaygaHoshi@users.noreply.github.com>
2022-05-03 03:55:50 +0300

    Fix #3

--- app/src/main/java/com/example/givasong/MainActivity.kt
+++ app/src/main/java/com/example/givasong/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.givasong
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -10,9 +9,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var musics: ArrayList<Music>
+    private lateinit var musics: MutableList<Music>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onResume()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
         setContentView(R.layout.activity_main)
 
         // get recycler view into variable
@@ -21,19 +28,17 @@ class MainActivity : AppCompatActivity() {
         // get FAB into variable
         val fabToAddMusic: FloatingActionButton = findViewById(R.id.floatingActionButton) //startActivity(Intent(this@MainActivity, OtherActivity::class.java))
 
-        fabToAddMusic.setOnClickListener { view ->
-
+        fabToAddMusic.setOnClickListener {
             val intent = Intent(this, AddForm::class.java).apply { }
             startActivity(intent)
-
-
-
-
         }
 
 
         // Initialize music
-        musics = Music.createMusicsList(20)
+        //// get musics from file
+        musics = getMusicsFromFile()
+        //musics = Music.createMusicsList(20)
+
         // Create adapter passing in the sample user data
         val adapter = MusicAdapter(musics)
         // Attach the adapter to the recyclerview to populate items
@@ -41,5 +46,41 @@ class MainActivity : AppCompatActivity() {
         // Set layout manager to position the items
         rvContacts.layoutManager = LinearLayoutManager(this)
     }
+
+
+    private fun getMusicsFromFile(): MutableList<Music> {
+
+        val temp: MutableList<Music> = ArrayList()
+        var id = 0
+
+        val sharedPref = getSharedPreferences("Saved", MODE_PRIVATE)
+        val musicsList = sharedPref.getString("Saved", "")
+        print("Musics: $musicsList\n\n\n\n")
+        if (musicsList == null || musicsList == ""){
+            temp += (Music(0, "00:00", "Add a new music!", ""))
+        }
+        else {
+            val musicsArray = musicsList.split("\n")
+            musicsArray.forEach {
+                if(it.contains(";")){
+                    print("it: $it \n\n\n")
+                    val musicLine = it.split(";;")
+                    val musicObjTemp = Music(id, musicLine[0], musicLine[1], musicLine[2])
+                    temp += (musicObjTemp)
+
+                    id += 1
+                }
+
+            }
+
+        }
+
+
+
+
+
+        return temp
+    }
+
 }
 
--- app/src/main/java/com/example/givasong/Music.kt
+++ app/src/main/java/com/example/givasong/Music.kt
@@ -1,17 +1,11 @@
 package com.example.givasong
 
-class Music (val duration: String, val artist: String, val name: String){
 
-    companion object {
-        private var lastMusicId = 0
-        fun createMusicsList(numMusics: Int): ArrayList<Music> {
-            val musics = ArrayList<Music>()
-            for (i in 1..numMusics) {
-                musics.add(Music("01:00", "Person " + ++lastMusicId, "Music $lastMusicId"))
-            }
-            return musics
-        }
-    }
+class Music (idNew: Int, durationNew: String, artistNew: String, nameNew: String){
 
+    val id = idNew
+    val duration = durationNew
+    val artist = artistNew
+    val name = nameNew
 
 }
